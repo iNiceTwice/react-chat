6a7e8f5efa3cf3e6f6d3800bcf94d87d0a6617c9
NiceTwice <onlynicetwice@gmail.com>
2023-02-14 18:11:41 -0300

    adding text to heading

--- client/src/components/Heading.tsx
+++ client/src/components/Heading.tsx
@@ -1,7 +1,13 @@
 const Heading = () => {
     return ( 
-        <div className="w-full py-14 border-b bg-secondary">
-
+        <div className="flex w-full h-32 gap-6 items-center border-b bg-secondary">
+            <div className=" ml-6">
+                <div className="p-5 rounded-full bg-primary"></div>
+            </div>
+            <div className="flex flex-col">
+                <h2 className="text-2xl font-medium text-slate-800/80">Marcos T.</h2>
+                <p className="text-green-500">Online</p>
+            </div>
         </div>
      );
 }
