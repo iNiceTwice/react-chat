14f63e5aeafd34c7dcccb012698af5f83140d882
NiceTwice <onlynicetwice@gmail.com>
2023-02-21 20:14:15 -0300

    add icons to side menu

--- client/src/components/chat/SideMenu.tsx
+++ client/src/components/chat/SideMenu.tsx
@@ -1,4 +1,5 @@
 import Logo from "../../assets/logo.png"
+import { CiChat1, CiSettings, CiFolderOn, CiUser, CiLogout } from "react-icons/ci"
 
 const SideMenu = () => {
     return ( 
@@ -6,14 +7,26 @@ const SideMenu = () => {
             <button className="mt-12">
                 <img src={Logo} width={30} className="hover:drop-shadow-xl"/>
             </button>
-            <div className="flex flex-col gap-10">
-                <div className="p-2 bg-slate-200"></div>
-                <div className="p-2 bg-slate-200"></div>
-                <div className="p-2 bg-slate-200"></div>
-                <div className="p-2 bg-slate-200"></div>
-                <div className="p-2 bg-slate-200"></div>
-                <div className="p-2 bg-slate-200"></div>
-                <div className="p-2 bg-slate-200"></div>
+            <div className="flex flex-col gap-6">
+                <button className="relative p-3 rounded-full hover:bg-primary hover:text-white text-slate-400 hover:shadow-sm transition-colors">
+                    <div className="flex items-center justify-center">
+                        <CiUser className="-ml-1" size={25}/>
+                        <span className="absolute top-2 left-[60%]">+</span>
+                    </div>
+                </button>
+                <button className="p-3 rounded-full hover:bg-primary hover:text-white text-slate-400 hover:shadow-sm transition-colors">
+                    <CiFolderOn size={25}/>
+                </button>
+                <button className="p-3 rounded-full hover:bg-primary hover:text-white text-slate-400 hover:shadow-sm transition-colors">
+                    <CiChat1 size={25}/>
+                </button>
+                <button className="p-3 rounded-full hover:bg-primary hover:text-white text-slate-400 hover:shadow-sm transition-colors">
+                    <CiLogout size={25}/>
+                </button>
+                <button className="p-3 rounded-full hover:bg-primary hover:text-white text-slate-400 hover:shadow-sm transition-colors">
+                    <CiSettings size={25}/>
+                </button>
+
             </div>
             <div className="flex flex-col mb-12">
                 <div className="p-4 rounded-full bg-primary"></div>
--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -20,6 +20,10 @@ const UserSchema = new mongoose.Schema({
         type:String,
         required:true,
         trim:true
+    },
+    contacts:{
+        type:Array,
+        required:false,
     }
 },{
     timestamps:true
