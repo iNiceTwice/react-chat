ddb0c3d63b00ee3a4d6b817ca3cd4cec0b43a519
NiceTwice <onlynicetwice@gmail.com>
2023-03-16 15:07:37 -0300

    add close button to ContactProfile

--- client/src/components/chat/ContactProfile.tsx
+++ client/src/components/chat/ContactProfile.tsx
@@ -1,14 +1,22 @@
 import { ChatContext } from "../../context/chat/chatContext";
 import { useContext } from "react"
 import UserProfile from "./UserProfile";
+import { IoCloseOutline } from "react-icons/io5";
 
 const ContactProfile = () => {
 
-    const { state } = useContext(ChatContext)
+    const { state, setState } = useContext(ChatContext)
+
+    const handleClick = () => {
+        setState(prev => ({...prev, showContactProfile:false}))
+    }
 
     if(state.showContactProfile){
         return ( 
-            <div className="lg:flex flex-col md:w-[22rem] md:border-l-2 border-slate-100 bg-secondary w-full">
+            <div className="relative lg:flex flex-col md:w-[22rem] md:border-l-2 border-slate-100 bg-secondary w-full">
+                <button onClick={handleClick} className="absolute z-20 top-0 m-6">
+                    <IoCloseOutline className="text-white" size={30} />
+                </button>
                 <UserProfile
                     username={state.currentConversation.contactName}
                     img={state.currentConversation.contactImage}
