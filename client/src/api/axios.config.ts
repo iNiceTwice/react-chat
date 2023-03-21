ecd431d5979b17dedecf1e26f0f9c58a0a2bcb5e
NiceTwice <onlynicetwice@gmail.com>
2023-03-20 22:40:48 -0300

    new url to server at clietn files

--- client/src/api/axios.config.ts
+++ client/src/api/axios.config.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const instance = axios.create({
    withCredentials: true,
-   baseURL: "http://localhost:3001"
+   baseURL: "https://chatio-server.up.railway.app"
 })
 
 export default instance
